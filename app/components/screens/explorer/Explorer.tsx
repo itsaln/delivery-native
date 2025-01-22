5f2aa1d9ab30c42ff87b37b63dac2fc5bc3949c9
Aladin Mukhammadiev <itsaln23@gmail.com>
2025-01-22 12:08:41 +0500

    explorer

--- app/components/screens/explorer/Explorer.tsx
+++ app/components/screens/explorer/Explorer.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+
+import Layout from '@/components/layout/Layout'
+import Loader from '@/components/ui/Loader'
+import Catalog from '@/components/ui/catalog/Catalog'
+
+import { useGetAllProducts } from './useGetAllProducts'
 
 const Explorer: FC = () => {
+	const { products, isLoading } = useGetAllProducts()
+
 	return (
-		<View>
-			<Text>Explorer</Text>
-		</View>
+		<Layout>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<Catalog title='Explorer' products={products || []} />
+			)}
+		</Layout>
 	)
 }
 
